5475042c5d9c7c47f9fc9d06ff7a58cba054bcd5
yakivyusin <yusin.yakiv@gmail.com>
2019-07-21 13:49:23 +0300

    Added Corpus Model Class

---
+++ IslandClusteringAcceleration/Models/Corpus.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IslandClusteringAcceleration.Models
+{
+    public class Corpus
+    {
+        private Text[] _texts;
+        private Lazy<IReadOnlyCollection<string>> _allLemmas;
+        private Lazy<IReadOnlyCollection<string>> _uniqueLemmas;
+
+        public Corpus(IEnumerable<Text> texts)
+        {
+            _texts = texts.ToArray();
+            _allLemmas = new Lazy<IReadOnlyCollection<string>>(() => _texts.SelectMany(x => x.Lemmas).ToList());
+            _uniqueLemmas = new Lazy<IReadOnlyCollection<string>>(() => _texts.SelectMany(x => x.Lemmas).Distinct().ToList());
+        }
+
+        public IReadOnlyCollection<Text> Texts => _texts;
+
+        public IReadOnlyCollection<string> AllLemmas => _allLemmas.Value;
+
+        public IReadOnlyCollection<string> UniqueLemmas => _uniqueLemmas.Value;
+    }
+}
