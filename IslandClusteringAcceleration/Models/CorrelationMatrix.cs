ed225c237f9525c30b30f47e76ab198aa80172b4
yakivyusin <yusin.yakiv@gmail.com>
2019-07-21 13:57:50 +0300

    Added Correlation Matrix Model Class

---
+++ IslandClusteringAcceleration/Models/CorrelationMatrix.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace IslandClusteringAcceleration.Models
+{
+    public class CorrelationMatrix
+    {
+        private int _dimension;
+        private double[] _valueVector;
+
+        public CorrelationMatrix(int dimension)
+        {
+            _dimension = dimension;
+            _valueVector = new double[_dimension * (_dimension - 1) / 2];
+        }
+
+        public double this[int i, int j]
+        {
+            get => _valueVector[GetPositionInVector(i, j)];
+            set => _valueVector[GetPositionInVector(i, j)] = value;
+        }
+
+        private int GetPositionInVector(int i, int j)
+        {
+            if (i == j)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            if (i > j)
+            {
+                var temp = i;
+                i = j;
+                j = temp;
+            }
+
+            return _dimension * (_dimension - 1) / 2 - (_dimension - i) * (_dimension - i - 1) / 2 + j - i - 1;
+        }
+    }
+}
