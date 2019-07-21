69985fdbaeeb4be2864042f419d7662a4702d957
yakivyusin <yusin.yakiv@gmail.com>
2019-07-21 13:43:54 +0300

    Added Text Model Class

---
+++ IslandClusteringAcceleration/Models/Text.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace IslandClusteringAcceleration.Models
+{
+    public class Text
+    {
+        private string[] _lemmas;
+        
+        public Text(IEnumerable<string> lemmas)
+        {
+            _lemmas = lemmas.ToArray();
+        }
+
+        public IReadOnlyCollection<string> Lemmas => _lemmas;
+    }
+}
