ac434bb156accb460e43e4f1b54773435f83f048
yakivyusin <yusin.yakiv@gmail.com>
2019-07-23 19:06:39 +0300

    Added Parallel2 Cycle Provider

---
+++ IslandClusteringAcceleration/CycleProviders/Parallel2.cs
@@ -0,0 +1,20 @@
+﻿using IslandClusteringAcceleration.Contracts;
+using System;
+using Tpl = System.Threading.Tasks.Parallel;
+
+namespace IslandClusteringAcceleration.CycleProviders
+{
+    public class Parallel2 : ICycleProvider
+    {
+        public void Run(int matrixDimension, Action<int, int> loopAction)
+        {
+            Tpl.For(0, matrixDimension, (int i) =>
+            {
+                Tpl.For(i + 1, matrixDimension, (int j) =>
+                {
+                    loopAction(i, j);
+                });
+            });
+        }
+    }
+}
