e6cbef738606481879482ac59d2d20532927ae98
yakivyusin <yusin.yakiv@gmail.com>
2019-07-23 19:02:31 +0300

    Added Serial Cycle Provider

---
+++ IslandClusteringAcceleration/CycleProviders/Serial.cs
@@ -0,0 +1,19 @@
+﻿using IslandClusteringAcceleration.Contracts;
+using System;
+
+namespace IslandClusteringAcceleration.CycleProviders
+{
+    public class Serial : ICycleProvider
+    {
+        public void Run(int matrixDimension, Action<int, int> loopAction)
+        {
+            for (int i = 0; i < matrixDimension; i++)
+            {
+                for (int j = i + 1; j < matrixDimension; j++)
+                {
+                    loopAction(i, j);
+                }
+            }
+        }
+    }
+}
