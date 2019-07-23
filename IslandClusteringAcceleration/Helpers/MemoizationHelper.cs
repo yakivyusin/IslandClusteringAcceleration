08514de3a229677aaea5d293bac29d96aeac0e64
yakivyusin <yusin.yakiv@gmail.com>
2019-07-23 19:13:41 +0300

    Added Memoization Helper

---
+++ IslandClusteringAcceleration/Helpers/MemoizationHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IslandClusteringAcceleration.Helpers
+{
+    internal class MemoizationHelper<TKey, TValue>
+    {
+        private Dictionary<TKey, TValue> _memoizedValues = new Dictionary<TKey, TValue>();
+
+        public TValue GetValue(TKey key, Func<TKey, TValue> calculationCallback)
+        {
+            if (!_memoizedValues.TryGetValue(key, out TValue value))
+            {
+                value = calculationCallback(key);
+                _memoizedValues.Add(key, value);
+            }
+
+            return value;
+        }
+    }
+}
