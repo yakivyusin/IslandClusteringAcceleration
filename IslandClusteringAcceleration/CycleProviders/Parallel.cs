13cdf1a87fa6da95d604bae0764e588178d81472
yakivyusin <yusin.yakiv@gmail.com>
2019-07-23 19:05:00 +0300

    Added Parallel Cycle Provider

---
+++ IslandClusteringAcceleration/CycleProviders/Parallel.cs
@@ -0,0 +1,20 @@
+﻿using IslandClusteringAcceleration.Contracts;
+using System;
+using Tpl = System.Threading.Tasks.Parallel;
+
+namespace IslandClusteringAcceleration.CycleProviders
+{
+    public class Parallel : ICycleProvider
+    {
+        public void Run(int matrixDimension, Action<int, int> loopAction)
+        {
+            Tpl.For(0, matrixDimension, (int i) =>
+            {
+                for (int j = i + 1; j < matrixDimension; j++)
+                {
+                    loopAction(i, j);
+                }
+            });
+        }
+    }
+}
