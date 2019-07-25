ed8a6c18f090ae62e99649554b3dc382818895a4
yakivyusin <yusin.yakiv@gmail.com>
2019-07-25 20:03:14 +0300

    Added Calculus Term Occurrences Count Provider

---
+++ IslandClusteringAcceleration/TermOccurrenceCountProviders/Calculus.cs
@@ -0,0 +1,16 @@
+﻿using IslandClusteringAcceleration.Contracts;
+using IslandClusteringAcceleration.Models;
+using System.Linq;
+
+namespace IslandClusteringAcceleration.TermOccurrenceCountProviders
+{
+    public class Calculus : ITermOccurrenceCountProvider
+    {
+        public virtual int GetCount(Corpus corpus, int j)
+        {
+            return corpus.
+                AllLemmas.
+                Count(x => x == corpus.UniqueLemmas.ElementAt(j));
+        }
+    }
+}
