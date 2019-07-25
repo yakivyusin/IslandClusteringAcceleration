791dc26bed9df9cedf8cf1eb6a467ffd9f871114
yakivyusin <yusin.yakiv@gmail.com>
2019-07-25 20:06:52 +0300

    Added Memoized Term Occurrences Count Provider

---
+++ IslandClusteringAcceleration/TermOccurrenceCountProviders/Memoized.cs
@@ -0,0 +1,15 @@
+﻿using IslandClusteringAcceleration.Helpers;
+using IslandClusteringAcceleration.Models;
+
+namespace IslandClusteringAcceleration.TermOccurrenceCountProviders
+{
+    public class Memoized : Calculus
+    {
+        private MemoizationHelper<int, int> _memoizationHelper = new MemoizationHelper<int, int>();
+
+        public override int GetCount(Corpus corpus, int j)
+        {
+            return _memoizationHelper.GetValue(j, (index) => base.GetCount(corpus, index));
+        }
+    }
+}
