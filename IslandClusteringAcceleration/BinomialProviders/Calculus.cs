b259bd8eb5252fab0bd894a2291c6e1b0d43ec53
yakivyusin <yusin.yakiv@gmail.com>
2019-07-25 20:24:52 +0300

    Added Calculus Binomial Provider

---
+++ IslandClusteringAcceleration/BinomialProviders/Calculus.cs
@@ -0,0 +1,34 @@
+﻿using IslandClusteringAcceleration.Contracts;
+
+namespace IslandClusteringAcceleration.BinomialProviders
+{
+    public class Calculus : IBinomialProvider
+    {
+        public double GetCoefficient(double n, double k)
+        {
+            if (k > n)
+            {
+                return 0;
+            }
+
+            if (n == k)
+            {
+                return 1;
+            }
+
+            if (k > n - k)
+            {
+                k = n - k;
+            }
+
+            double result = 1;
+            for (double i = 1; i <= k; i++)
+            {
+                result *= n--;
+                result /= i;
+            }
+
+            return result;
+        }
+    }
+}
