2aa7fa35907a029878ebf79028b5269d68a01dfe
yakivyusin <yusin.yakiv@gmail.com>
2019-07-25 20:13:48 +0300

    Added Calculus Term Count in Texts Provider

---
+++ IslandClusteringAcceleration/TermCountInTextsProviders/Calculus.cs
@@ -0,0 +1,17 @@
+﻿using IslandClusteringAcceleration.Contracts;
+using IslandClusteringAcceleration.Models;
+using System.Linq;
+
+namespace IslandClusteringAcceleration.TermCountInTextsProviders
+{
+    public class Calculus : ITermCountInTextsProvider
+    {
+        public int GetCount(Corpus corpus, int i)
+        {
+            return corpus.Texts.
+                Where(x => x.Lemmas.Contains(corpus.UniqueLemmas.ElementAt(i))).
+                SelectMany(x => x.Lemmas).
+                Count();
+        }
+    }
+}
