077beb8ebe76cdddb5f424958d312ab40f4fc407
yakivyusin <yusin.yakiv@gmail.com>
2019-08-04 16:08:26 +0300

    Added Benchmark Project Scratch

---
+++ IslandClusteringAcceleration.Benchmark/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IslandClusteringAcceleration.Benchmark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
