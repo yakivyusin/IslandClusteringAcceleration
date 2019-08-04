248d94106affaa2cee070453cae4cc8aca41702b
yakivyusin <yusin.yakiv@gmail.com>
2019-08-04 17:37:19 +0300

    Added Simple Lock for Memoization Helper

--- IslandClusteringAcceleration/Helpers/MemoizationHelper.cs
+++ IslandClusteringAcceleration/Helpers/MemoizationHelper.cs
@@ -6,13 +6,21 @@ namespace IslandClusteringAcceleration.Helpers
     internal class MemoizationHelper<TKey, TValue>
     {
         private Dictionary<TKey, TValue> _memoizedValues = new Dictionary<TKey, TValue>();
+        private object _lock = new object();
 
         public TValue GetValue(TKey key, Func<TKey, TValue> calculationCallback)
         {
             if (!_memoizedValues.TryGetValue(key, out TValue value))
             {
                 value = calculationCallback(key);
-                _memoizedValues.Add(key, value);
+
+                lock (_lock)
+                {
+                    if (!_memoizedValues.ContainsKey(key))
+                    {
+                        _memoizedValues.Add(key, value);
+                    }
+                }
             }
 
             return value;
